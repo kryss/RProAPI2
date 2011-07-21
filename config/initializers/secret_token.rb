2a9f4b0905662ab5847ca046ce47f654f72e789a
Kryss <kryss.col@gmail.com>
2011-07-21 15:20:39 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RProAPI2::Application.config.secret_token = '0602b98df2406d40dff1ea423156ab1ba4e7af41d8ca7a4eaa94cebb46440f868aad7030033a4710635e9b9392259a927f7d11967893a7cd330ad23bbbaaec6f'
